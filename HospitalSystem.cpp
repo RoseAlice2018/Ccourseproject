8fd4270aec9fc54854e2da4d66af1ef38cd9b24f
RoseAlice2018 <40765813+RoseAlice2018@users.noreply.github.com>
2020-09-06 12:03:40 +0800

    做了一点小的改动

--- HospitalSystem.cpp
+++ HospitalSystem.cpp
@@ -5,20 +5,6 @@ int countDepart=0;// department数量
 MedicalRecords* records;
 MedicalRecords* pointer=records;
 depart RealDepartment[30];
-struct MedicalRecords
-{
-    PatientInformation paIn;
-    DoctorInformation  doIn;
-    MedicalInformation mdIn;
-    MedicalRecords* before;
-    MedicalRecords* next;
-};
-struct depart
-{
-    char name[30];// name of the department
-};
-
-
 void insertDepartment(char insertname[30])
 {
     for(int i=0;i<countDepart;i++)
@@ -39,9 +25,9 @@ void AddMedicalRecords()
     scanf("%d",&tag);
     if(tag==1)
     {
-
+        // 从文件读入诊疗记录
     }
-    else if(tag==2)
+    else if(tag==2)// 选择手动输入诊疗记录
     {
         MedicalRecords* temp=new MedicalRecords();
         printf("请您按标准格式输入如下信息\n");
@@ -52,16 +38,51 @@ void AddMedicalRecords()
         //插入department
         insertDepartment(temp->doIn.department);
         // 输入出诊时间
-        printf("请您输入诊疗情况：\n");
-        printf("请您输入检查状况---格式如下：100 200 依次输入每次检查费用 如未有检查请输入0");
+
         //输入检查状况
-        printf("请您输入开药记录：\n");
-        printf("请您输入开药记录---格式如下：药品名称 单价 数量\n");
+        printf("请您输入诊疗情况：\n");
+        printf("请您输入检查次数：如未有检查请输入0\n");
+        int checkNumber=0;
+        scanf("%d",&checkNumber);
+        if(checkNumber==0)
+        {
+            printf("您未有检查记录\n");
+        }
+        else
+        {
+            printf("请依次输入每次检查费用---格式如下：100 ");
+            while (checkNumber--)
+            {
+                int c;
+                scanf("%d",&c);
+                temp->mdIn.exam.cost[temp->mdIn.exam.costcount++]=c;
+                temp->mdIn.exam.SumCost+=c;
+            }  
+        }
         //输入开药记录
+        printf("请您输入开药次数：如未曾开药，请输入0\n");
+        int medicount;
+        scanf("%d",&medicount);
+        if(medicount==0)
+        {
+            printf("您未曾有开药记录.\n");
+        }
+        else
+        {
+            while(medicount--)
+            {
+                printf("请您输入开药记录---格式如下：药品名称 单价 数量\n");
+                scanf("%s %d %d",temp->mdIn.prmd.drug[temp->mdIn.prmd.drugcount]->name,&temp->mdIn.prmd.drug[temp->mdIn.prmd.drugcount]->UnitPrice,&temp->mdIn.prmd.drug[temp->mdIn.prmd.drugcount]->Number);
+                 temp->mdIn.prmd.SumDrugCost+=temp->mdIn.prmd.drug[temp->mdIn.prmd.drugcount++].cost;
+            }
+            
+        }
+        //输入住院记录
         printf("请您输入住院记录---格式如下：（住院开始记录）月/日/时/分 （预期出院日期）月/日/时/分 住院押金\n");
         scanf("%d/%d/%d/%d %d/%d/%d/%d %d",&temp->mdIn.hosp.beginDate.month,&temp->mdIn.hosp.beginDate.day
         ,&temp->mdIn.hosp.beginDate.hour,&temp->mdIn.hosp.beginDate.minute,&temp->mdIn.hosp.leaveDate.month,
         &temp->mdIn.hosp.leaveDate.day,&temp->mdIn.hosp.leaveDate.hour,&temp->mdIn.hosp.leaveDate.minute,&temp->mdIn.hosp.deposit);
+        //将当前记录链入表中
         pointer=temp;
         pointer=pointer->next;
     }
